bc8aac57b91882cb00ffd79ce4c76c7d5259dfbf
shaoxi-fighting <2509118314@qq.com>
2021-05-22 19:21:34 +0800

    完成用户信息开发

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,57 @@
+$(function () {
+
+    var form = layui.form
+    var layer = layui.layer
+    form.verfiy({
+        nickname: function (value) { 
+            if(value.length>6){
+                return '昵称长度必须在1~6个字符之间'
+            }
+         }
+    })
+
+    initUserInfo()
+
+    // 初始化用户的基本信息
+    function initUserInfo() {
+      $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function(res) {
+          if (res.status !== 0) {
+            return layer.msg('获取用户信息失败！')
+          }
+          // console.log(res)
+          // 调用 form.val() 快速为表单赋值
+          form.val('formUserInfo', res.data)
+        }
+      })
+    }
+    // 监听表单提交事件
+
+    $('.layui-form').on('submit',function (e) { 
+        // 阻止默认行为
+        e.preventDefault()
+        // 发起请求
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function (res) { 
+                if(res.status!==0){
+                    return layer.msg('更新用户信息失败！')
+                }
+                layer.msg('更新用户信息成功')
+                // 调用父页面的方法，重新渲染用户信息
+                window.parent.getuserinfo()
+             }
+        })
+     })
+
+    //  重置表单数据
+     $('#btnReset').on('click',function (e) {
+         e.preventDefault()
+         initUserInfo()
+       })
+
+})
